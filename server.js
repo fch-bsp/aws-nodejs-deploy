55b28abd1e766494048b5e1c5ba1d71c79e432a0
Fernando Horas <fernando.horas@fchdigital.com.br>
2024-04-02 15:03:48 -0300

    new server

--- server.js
+++ server.js
@@ -7,22 +7,23 @@ app.get('/api/get',(req,res) => {
 })
 
 
+
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"satyam",
+            age:22,
+            contact:12254
+        },env:process.env.NAME
+    })
+});
+
+
+
 app.listen(8000,()=> {
     console.log("listening to 8000");   
 })
 
-
-
-
-// app.get('/api/get_user_details',(req,res) => {
-//     res.send({
-//         user:{
-//             name:"satyam",
-//             age:22,
-//             contact:12254
-//         },env:process.env.NAME
-//     })
-// });
 
 
 // app.listen(process.env.PORT,() => {
