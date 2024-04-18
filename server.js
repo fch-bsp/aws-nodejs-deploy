a63da3112f8cb4ef1dcb77c3e5875ae217414175
Fernando Carlos Horas <nandohoras@gmail.com>
2024-04-18 19:48:59 -0300

    ajuste no server,js 02

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const videoEstilos = `
 app.get('/api/get_with_image_and_video', (req, res) => {
     res.send(`
         <div style="${estilos}">
-            <p>Esta é Topologia: versão 001</p>
+            <p>Esta é Topologia: versão 002</p>
             <p>Implantar projeto Node.js na Amazon EC2 com GitHub Actions:</p>
             <img src="https://lab-ec2-ci-cd.s3.us-east-2.amazonaws.com/Top.png" alt="Imagem de exemplo">
             <p>Aqui está um vídeo mostrando passo-passo da aplicação utilizando GitHub Actions:</p>
@@ -82,39 +82,3 @@ app.listen(process.env.PORT, () => {
 
 
 
-// versão 02
-
-// const express = require("express");
-// const app = express();
-// require("dotenv").config();
-
-// // Estilos de formatação adicionados para destacar a sintaxe
-// const estilos = `
-//     background-color: #f0f0f0; /* Cor de fundo */
-//     color: #333; /* Cor do texto */
-//     font-size: 16px; /* Tamanho da fonte */
-//     padding: 20px; /* Espaçamento interno */
-// `;
-
-// app.get('/api/get', (req, res) => {
-//     res.send('<div style="' + estilos + '">hello back to nodejs v2</div>');
-// });
-
-// app.get('/api/get_user_details', (req, res) => {
-//     res.send(`
-//         <div style="${estilos}">
-//             <p>User Details:</p>
-//             <ul>
-//                 <li>Name: horas</li>
-//                 <li>Age: 40</li>
-//                 <li>Contact: 11995143947</li>
-//             </ul>
-//             <p>Environment: ${process.env.NAME}</p>
-//             <li>Versao app: 0001</li>
-//         </div>
-//     `);
-// });
-
-// app.listen(process.env.PORT, () => {
-//     console.log("listening to 8000");
-// });
