0da025306c08f1682dafcfc3887530424fd75375
Fernando Carlos Horas <nandohoras@gmail.com>
2024-04-18 19:20:58 -0300

    ajuste no server,js

--- server.js
+++ server.js
@@ -40,3 +40,81 @@ app.listen(process.env.PORT, () => {
 
 
 
+
+// versão 02
+
+// const express = require("express");
+// const app = express();
+// require("dotenv").config();
+
+// // Estilos de formatação adicionados para destacar a sintaxe
+// const estilos = `
+//     background-color: #f0f0f0; /* Cor de fundo */
+//     color: #333; /* Cor do texto */
+//     font-size: 16px; /* Tamanho da fonte */
+//     padding: 20px; /* Espaçamento interno */
+// `;
+
+// app.get('/api/get', (req, res) => {
+//     res.send('<div style="' + estilos + '">hello back to nodejs v2</div>');
+// });
+
+// app.get('/api/get_user_details', (req, res) => {
+//     res.send(`
+//         <div style="${estilos}">
+//             <p>User Details:</p>
+//             <ul>
+//                 <li>Name: horas</li>
+//                 <li>Age: 40</li>
+//                 <li>Contact: 11995143947</li>
+//             </ul>
+//             <p>Environment: ${process.env.NAME}</p>
+//             <li>Versao app: 0001</li>
+//         </div>
+//     `);
+// });
+
+// app.listen(process.env.PORT, () => {
+//     console.log("listening to 8000");
+// });
+
+
+
+// versão 01
+
+
+// const express = require("express");
+// const app = express();
+// require("dotenv").config();
+
+// // Estilos de formatação adicionados para destacar a sintaxe
+// const estilos = `
+//     background-color: #000; /* Cor de fundo preto */
+//     color: #fff; /* Cor do texto branco */
+//     font-size: 16px; /* Tamanho da fonte */
+//     padding: 20px; /* Espaçamento interno */
+// `;
+// // http://3.18.20.84/api
+// app.get('/api/get', (req, res) => {
+//     res.send('<div style="' + estilos + '">hello back to nodejs v2</div>');
+// });
+
+// //http://3.18.20.84/api/get_user_details
+// app.get('/api/get_user_details', (req, res) => {
+//     res.send(`
+//         <div style="${estilos}">
+//             <p>User Details:</p>
+//             <ul>
+//                 <li>Name:Fernando Carlos horas</li>
+//                 <li>Idade: 43</li>
+//                 <li>Contato: 11995143947</li>
+//                 <li>Versão app: 001</li>                
+//             </ul>
+//             <p>.env: ${process.env.NAME}</p>
+//         </div>
+//     `);
+// });
+
+// app.listen(process.env.PORT, () => {
+//     console.log("listening to 8000");
+// });
